8da01981bb7cfd9b89bad28204b1b3ac2d90d219
Asim Roy Chowdhury <85804741+AgentCypher@users.noreply.github.com>
2022-07-06 23:08:35 +0530

    Create GRPASSN.java

---
+++ GRPASSN.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+class CodeChefCC {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        int x; 
+        long arr[];
+        while(t-- > 0){
+		x = sc.nextInt();
+		arr = new long[x];
+		boolean approve = false;
+		ArrayList<Long> arrli = new ArrayList<>();
+		for(int i=0; i<x; i++){
+			arr[i] = sc.nextLong();
+		}
+		for(int i =0; i<x;i++){
+			if(arrli.contains(arr[i]) == false){
+				arrli.add(arr[i]);
+			}
+		}
+		long newarr[] = new long[arrli.size()];
+		for(int i = 0; i<arrli.size(); i++){
+			newarr[i] = 0;
+			for(int j=0; j<x; j++){
+				if(arrli.get(i) == arr[j]){
+					newarr[i] += 1;
+				}
+			}
+		}
+		for(int i=0 ; i<arrli.size(); i++){
+			if(arrli.get(i) == newarr[i]){
+				approve = true;
+			}
+			else {
+				approve = false;
+				break;
+			}
+		}
+		if(approve == true) System.out.println("Yes");
+		else System.out.println("No");
+	    }
+	        
+    }         
+
+}          
